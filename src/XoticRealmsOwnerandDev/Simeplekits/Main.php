7ac2ce9c7c5e3bf19e639c93116b9d1aeffb7894
Blubberboy333 <thecowmooman@gmail.com>
2016-04-29 08:49:52 -0400

    Began re-writing to add new features

--- src/XoticRealmsOwnerandDev/Simeplekits/Main.php
+++ src/XoticRealmsOwnerandDev/Simeplekits/Main.php
@@ -13,49 +13,64 @@ use pocketmine\Player;
 class Main extends PluginBase{
 	public function onEnable(){
 		$this->saveDefaultConfig();
+		if(!(s_dir($this->getDataFolder()."Kits/"))){
+			mkdir($this->getDataFolder()."Kits/");
+			$this->getLogger()->info("Made a folder for the kits...");
+		}
+		foreach($this->getConfig()->get("List") as $i){
+			if(!(file_exists($this->getDataFolder()."Kits/".$i.".yml"))){
+				$newFile = new Config($this->getDataFolder()."Kits/".$i.".yml", Config::YAML);
+				$this->getLogger()->info("Made a file for the kit ".TextFormat::BLUE.$i);
+			}
+		}
 		$this->getLogger()->info(TextFormat::GREEN."Done!");
 	}
 	
+	public  addPlayer(Player $player, $kit){
+		$name = $player->getName();
+		$kit = new Config($this->getDataFolder()."Kits/".$kit.".yml", Config::YAML);
+		$kit->set($name, "Allowed");
+	}
+	
+	public function removePlayer(Player $player, $kit){
+		$name = $player->getName();
+		$kit = new Config($this->getDataFolder()."Kits/".$kit.".yml", Config::YAML);
+		$kit->set($name, "Removed");
+	}
+	
+	public function checkPlayer(Player $player, $kit){
+		$name = $player->getName();
+		$kit = new Config($this->getDataFolder()."Kits/".$kit.".yml", Config::YAML);
+		if(isset($kit->get($name))){
+			if($kit->get($name) == "Allowed"){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-		if(strtolower($command->getName()) == "kit"){
-			if($sender->hasPermission("kit") || $sender->hasPermission("kit.use")){
-				if(isset($args[0])){
-					if($args[0] == "list"){
-						$sender->sendMessage(implode(", ", $this->getConfig()->get("List")));
-						return true;
-					}else{
-						if($sender instanceof Player){
-							if($this->getConfig()->get($args[0]) !== null){
-								foreach($this->getConfig()->get($args[0]) as $i){
-									$item = Item::get($i);
-									if($item instanceof Item){
-										$sender->getInvntory()->addItem($item);
-									}else{
-										$this->getLogger()->info(TextFormat::RED.$i." is not an item!!! Please fix this!");
-									}
-								}
-								foreach($this->getConfig()->get("Commands") as $i){
-									$this->getServer()->dispatchCommand(new ConsoleCommandSender(str_replace(array("{PLAYER}", "{NAME}"), $sender, $sender->getName())));
-								}
-								$sender->sendMessage("You have chosen the kit ".$args[0]);
-								$this->getLogger()->info("[".$sender->getName()." is using the kit called ".$args[0]."]");
-								return true;
-							}else{
-								$sender->sendMessage(TextFormat::RED."There is no kit by that name!");
-								return true;
+		switch(strtolower($command->getName())){
+			case "kit":
+				if($sender->hasPermission("kit") || $sender->hasPermission("kit.use")){
+					if(isset($args[0])){
+						if($args[0] == "list"){
+							foreach($this->getConfig()->get("List") as $i){
+								$sender->sendMessage($i);
 							}
 						}else{
-							$sender->sendMessage(TextFormat::YELLOW."That command must be used in the game!");
-							return true;
+							if(isset($this->getConfig()->get($args[0]))){
+								if($this->checkPlayer($sender, $args[0]) == true){
+									$sender->sendMessage("You have chosen the kit ".$args[0]);
+									// Left off here...
+								}
+							}
 						}
 					}
-				}else{
-					return false;
 				}
-			}else{
-				$sender->sendMessage(TextFormat::RED."You don't have permission to use this command!");
-				return true;
-			}
 		}
 	}
 }
